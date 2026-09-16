196560dc33a22914a8d94cc146371af222398cd1
ateveraz <alejandro.tevera@cinvestav.mx>
2026-09-16 16:38:15 +0200

    Fix: Remove acceleration element to avoid algebraic loop

--- general/initBus_quadrotor_state.m
+++ general/initBus_quadrotor_state.m
@@ -3,8 +3,19 @@ function initBus_quadrotor_state()
 
 % Bus object: quadrotor_state 
 clear elems;
+% elems(1) = Simulink.BusElement;
+% elems(1).Name = 'linear_acceleration';
+% elems(1).Dimensions = 3;
+% elems(1).DimensionsMode = 'Fixed';
+% elems(1).DataType = 'double';
+% elems(1).Complexity = 'real';
+% elems(1).Min = [];
+% elems(1).Max = [];
+% elems(1).DocUnits = '';
+% elems(1).Description = '';
+
 elems(1) = Simulink.BusElement;
-elems(1).Name = 'linear_acceleration';
+elems(1).Name = 'linear_velocity';
 elems(1).Dimensions = 3;
 elems(1).DimensionsMode = 'Fixed';
 elems(1).DataType = 'double';
@@ -15,7 +26,7 @@ elems(1).DocUnits = '';
 elems(1).Description = '';
 
 elems(2) = Simulink.BusElement;
-elems(2).Name = 'linear_velocity';
+elems(2).Name = 'position';
 elems(2).Dimensions = 3;
 elems(2).DimensionsMode = 'Fixed';
 elems(2).DataType = 'double';
@@ -26,7 +37,7 @@ elems(2).DocUnits = '';
 elems(2).Description = '';
 
 elems(3) = Simulink.BusElement;
-elems(3).Name = 'position';
+elems(3).Name = 'angular_velocity';
 elems(3).Dimensions = 3;
 elems(3).DimensionsMode = 'Fixed';
 elems(3).DataType = 'double';
@@ -37,8 +48,8 @@ elems(3).DocUnits = '';
 elems(3).Description = '';
 
 elems(4) = Simulink.BusElement;
-elems(4).Name = 'angular_velocity';
-elems(4).Dimensions = 3;
+elems(4).Name = 'quaternion';
+elems(4).Dimensions = 4;
 elems(4).DimensionsMode = 'Fixed';
 elems(4).DataType = 'double';
 elems(4).Complexity = 'real';
@@ -46,17 +57,6 @@ elems(4).Min = [];
 elems(4).Max = [];
 elems(4).DocUnits = '';
 elems(4).Description = '';
-
-elems(5) = Simulink.BusElement;
-elems(5).Name = 'quaternion';
-elems(5).Dimensions = 4;
-elems(5).DimensionsMode = 'Fixed';
-elems(5).DataType = 'double';
-elems(5).Complexity = 'real';
-elems(5).Min = [];
-elems(5).Max = [];
-elems(5).DocUnits = '';
-elems(5).Description = '';
 
 quadrotor_state = Simulink.Bus;
 quadrotor_state.HeaderFile = '';
